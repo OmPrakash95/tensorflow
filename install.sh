81c4ce4b2d162eea1accd799525ec14b0da82ec5
William Chan <williamchan@cmu.edu>
2015-12-25 15:09:26 -0800

    work

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo apt-get install -y python-numpy swig python-dev
 wget http://developer.download.nvidia.com/compute/cuda/7_0/Prod/local_installers/cuda_7.0.28_linux.run
 
 sudo apt-get install -y python-pip python-dev python-virtualenv
-virtualenv --system-site-packages ~/tensorflow
+virtualenv --system-site-packages /data-local/wchan/tensorflow_env
